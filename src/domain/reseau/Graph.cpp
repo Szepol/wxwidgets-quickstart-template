a10e83f05134e7e3d2ae3e8c0446a411899e4338
Szepol <lmlsa1794@gmail.com>
2022-01-25 23:47:43 -0500

    Added documentation for the graph class

--- src/domain/reseau/Graph.cpp
+++ src/domain/reseau/Graph.cpp
@@ -57,35 +57,36 @@ bool Graph::RenameNode(Node* p_src, const std::string& p_name)
     return false;
 }
 /**
- * \brief 
+ * \brief Method to creates a new node and add an arcs from a source node to the newly created node
  * 
- * \param p_src
- * \param p_name
- * \param p_x
- * \param p_y
- * \param p_length
- * \param p_cost
- * \return 
+ * \param p_src Node* pointer to the source node of the arc
+ * \param p_name String name of the new node to point toward
+ * \param p_x Position along the horizontal axis of the destination node
+ * \param p_y Position along the vertical axis of the destination node
+ * \param p_duration Float duration in hours of the trip
+ * \param p_cost Float cost in dollars of the trip
+ * \return bool if an error was encountered during the process (true means there was an error)
  */
-bool Graph::AddNode(Node* p_src, const std::string& p_name, int p_x, int p_y, float p_length, float p_cost)
+bool Graph::AddNode(Node* p_src, const std::string& p_name, int p_x, int p_y, float p_duration, float p_cost)
 {
     // If the name is alreadly taken or the source node isn't in the graph return true;
     if (GetNode(p_name) != nullptr || !NodeExists(p_src))
         return true;
     Node* node(new Node(p_name, p_x, p_y));
     m_vNodes.push_back(node);
-    p_src->AddArc(node, p_length, p_cost);
+    p_src->AddArc(node, p_duration, p_cost);
     m_nArcs++;
     return false;
 }
 /**
- * \brief .
+ * \brief Method to add a directed arc from a node source pointing at a node destination, 
+ * specifying the duration and cost
  * 
- * \param p_src
- * \param p_dest
- * \param p_length
- * \param p_cost
- * \return 
+ * \param p_src Node* pointer to the source node
+ * \param p_dest Node* pointer to the destination node
+ * \param p_duration Float duration in hours of the trip
+ * \param p_cost Float cost in dollars of the trip
+ * \return bool if an error was encountered during the process (true means there was an error)
  */
 bool Graph::AddArc(Node* p_src, Node* p_dest, float p_length, float p_cost)
 {
@@ -97,11 +98,11 @@ bool Graph::AddArc(Node* p_src, Node* p_dest, float p_length, float p_cost)
     return false;
 }
 /**
- * \brief .
+ * \brief Method to remove the directed arc from source node poiting toward destination node.
  * 
- * \param p_src
- * \param p_dest
- * \return 
+ * \param p_src Node* pointer to the source node
+ * \param p_dest Node* pointer to the destination node
+ * \return bool if an error was encountered during the process (true means there was an error)
  */
 bool Graph::RemoveArc(Node* p_src, Node* p_dest)
 {
@@ -112,11 +113,11 @@ bool Graph::RemoveArc(Node* p_src, Node* p_dest)
     return false;
 }
 /**
- * \brief .
+ * \brief Method that returns if arc from source node pointing to destination node exists.
  * 
- * \param p_src
- * \param p_dest
- * \return 
+ * \param p_src Node* pointer to the source node
+ * \param p_dest Node* pointer to the destination node
+ * \return bool if the arc exists(true) or not (false)
  */
 bool Graph::ArcExists(Node* p_src, Node* p_dest) const
 {
@@ -125,72 +126,73 @@ bool Graph::ArcExists(Node* p_src, Node* p_dest) const
     return p_src->ArcExists(p_dest);
 }
 /**
- * \brief .
+ * \brief Method that returns the vector of nodes that the source node points to.
  * 
- * \param p_src
- * \return 
+ * \param p_src Node* pointer to the source node
+ * \return vector of Node pointers of all the nodes the source node is pointing to.
  */
 std::vector<Node*> Graph::GetAdjNodes(Node* p_src) const
 {
     return p_src->GetAdjNodes();
 }
 /**
- * \brief .
+ * \brief Method that returns the name of the node
  * 
- * \param p_src
- * \return 
+ * \param p_src Node* pointer to the source node
+ * \return String name of the node.
  */
 const std::string& Graph::GetNodeName(Node* p_src) const
 {
     return p_src->GetName();
 }
 /**
- * \brief .
+ * \brief Method to find node using its name, returns a pointer to the node <br>
+ * (returns nullptr when no node has the specified name)
  * 
- * \param p_nom
- * \return 
+ * \param p_name String name of the node to find
+ * \return Node* pointer to the unique node that has the specified name
  */
-Node* Graph::GetNode(const std::string& p_nom) const
+Node* Graph::GetNode(const std::string& p_name) const
 {
     auto it = std::find_if(m_vNodes.begin(), m_vNodes.end(), 
-        [&p_nom](const Node* arc) {return arc->GetName() == p_nom; });
+        [&p_name](const Node* arc) {return arc->GetName() == p_name; });
     if (it == m_vNodes.end())
         return nullptr;
     return (*it);
 }
 /**
- * \brief .
+ * \brief Method to get all the nodes in the graph
  * 
- * \return 
+ * \return vector of node pointers of all the nodes in the graph
  */
 std::vector<Node*> Graph::GetAllNodes() const
 {
     return m_vNodes;
 }
 /**
- * \brief .
+ * \brief Method that returns the number of node in the graph
  * 
- * \return 
+ * \return int number of node in the graph
  */
 int Graph::GetNodeCount() const
 {
     return m_vNodes.size();
 }
 /**
- * \brief .
+ * \brief Method that returns the number of arcs in the graph
  * 
- * \return 
+ * \return int number of arcs in the graph
  */
 int Graph::GetArcCount() const
 {
     return m_nArcs;
 }
 /**
- * \brief .
+ * \brief Method that returns the ponderations of arc 
  * 
- * \param p_src
- * \param p_dest
- * \return 
+ * \param p_src Node* pointer to the source node
+ * \param p_dest Node* pointer to the destination node
+ * \return Ponderations the ponderation object of the arc from source node pointing to destination node
  */
 Ponderations Graph::GetPonderationsArc(Node* p_src, Node* p_dest) const
 {
@@ -199,10 +201,10 @@ Ponderations Graph::GetPonderationsArc(Node* p_src, Node* p_dest) const
     return p_src->GetPonderation(p_dest);
 }
 /**
- * \brief .
+ * \brief privated method to get if node exists inside the graph
  * 
- * \param p_src
- * \return 
+ * \param p_src Node* pointer to the node to verify
+ * \return bool If the node exists returns true and false otherwise
  */
 bool Graph::NodeExists(Node* p_src) const
 {
--- src/domain/reseau/Graph.h
+++ src/domain/reseau/Graph.h
@@ -16,7 +16,6 @@
 #include <vector>
 #include <list>
 #include "Node.h"
-//Vous pouvez ajouter d'autres librairies de la STL
 
 namespace reseau_interurbain
 {
@@ -25,96 +24,38 @@ namespace domain
 
 /**
  * \class Graphe
- * \brief Classe représentant un graphe oriente
+ * \brief Classe representing a oriented graph<br>
+ * When constructing a graph object we also need to construct atleast one node object. <br>
+ * Once the graph is constructed it's possible tp add nodes and connect these nodes with oriented <br>
+ * arcs. Each arc has a ponderation.<br><br>
+ * 
+ * TODO: <br>
+ * Add a method to remove Node from the graph.<br>
+ * Change the class so that the graph stays valid at all time (shouldn't be able to remove an arc and leave a unconnected node)<br>
  *
- *      On y implemente un nombre de sommets lors de la construction, il est
- *      aussi possible de changer le nombre de sommet avec la methode resize,
- *      une fois le graphe construit, il est possible d'ajouter des arcs entre
- *      un sommet d'origine <b>a</b> vers un sommet destination <b>b</b>, chaque
- *      arc contient une ponderation.<br>
- *      L'index des sommets du graphe se trouve dans l'ensemble {0, ..., n-1}
- *      (ou n est le nombre de sommets).<br>
- *      <b>Attributs:</b><br>
- *      std::vector<std::string> <em>noms</em> Le vecteur qui contient l'ensemble des noms des sommets <br>
- *      std::vector<std::list<Arc>> <em>listesAdj</em> Le vecteur des listes d'adjacences de chaque sommets. <br>
- *      size_t <em>nbSommets</em> Entier positif (non strict) qui correspond au nombre de sommets <br>
- *      size_t <em>nbArcs</em> Entier positif (non strict) qui correspond au nombre d'arc <br>
- *      @throws std::logic_error <br>
  */
 class Graph
 {
 public:
-	// When creating a new graph, you create a single node by default, and thus we need its coordinate
 	Graph(const std::string& p_name, int p_x, int p_y);
-
 	~Graph();
 
-	// Rename an existing node inside the graph
 	bool RenameNode(Node* p_src, const std::string& p_name);
-
-	// Creates a new node and add an arcs from a source node to the newly created node
 	bool AddNode(Node* p_src, const std::string& p_name, int p_x, int p_y, float p_length, float p_cost);
-
-	// Adds a directed arc from a node source pointing at a node destination, specifying the
-	// length and cost
 	bool AddArc(Node* p_src, Node* p_dest, float p_length, float p_cost);
-
-	// Remove the directed arc from source node poiting toward destination node.
 	bool RemoveArc(Node* p_src, Node* p_dest);
-
-	// Vérifie si un arc existe
-	// Exception logic_error si source ou destination supérieur à nbSommets
 	bool ArcExists(Node* p_src, Node* p_dest) const;
-
-	// Return the vector of nodes that the source node points to.
 	std::vector<Node*> GetAdjNodes(Node* p_src) const;
-
-	// Return the name of the node
 	const std::string& GetNodeName(Node* sommet) const;
-
-	// Retourne le numéro d'un sommet
-	// Exception logic_error si nom n'existe pas dans le graphe
-	Node* GetNode(const std::string& nom) const;
-
+	Node* GetNode(const std::string& p_name) const;
 	std::vector<Node*> GetAllNodes() const;
-
-	// Return the number of nodes in the graph
 	int GetNodeCount() const;
-
-	// Return the number of arc in the graph
 	int GetArcCount() const;
-
-	// Return the ponderations of arc 
 	Ponderations GetPonderationsArc(Node* p_src, Node* p_dest) const;
 
-	/*//Ne touchez pas à cette fonction !
-	friend std::ostream& operator<<(std::ostream& out, const Graphe& g)
-	{
-		out << "Le graphe contient " << g.nbSommets << " sommet(s) et "
-			<< g.nbArcs << " arc(s)" << std::endl << std::endl;
-		for (size_t i = 0; i < g.nbSommets; ++i)
-		{
-			out << "Sommet " << g.noms[i] << ": " << std::endl << "Voisins: ";
-
-			if (g.listesAdj[i].size() == 0)
-				out << "Rien";
-			else
-			{
-				for (auto it = g.listesAdj[i].begin(); it != g.listesAdj[i].end(); ++it)
-					out << g.noms[it->destination] << ", ";
-			}
-			out << std::endl;
-		}
-		return out;
-	}
-	*/
-
 private:
-
 	std::vector<Node*> m_vNodes;
-
-	size_t m_nArcs;		// Number of arcs in the graph
-
+	size_t m_nArcs;
 
 	bool NodeExists(Node* p_src) const;
 };
